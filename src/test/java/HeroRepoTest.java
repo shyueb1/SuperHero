5184c3331940a576c3c1d57556a0f8605e166c4f
shyueb1 <32813614+shyueb1@users.noreply.github.com>
2020-09-22 14:39:47 +0100

    0014: Updated HeroRepoTest.java with some dummy tests

--- src/test/java/HeroRepoTest.java
+++ src/test/java/HeroRepoTest.java
@@ -1,5 +1,9 @@
-import com.sg.superherosightings.Data.HeroRepository;
+import com.sg.superherosightings.Data.*;
+import com.sg.superherosightings.Entity.JPAEntities.Hero;
+import com.sg.superherosightings.Entity.JPAEntities.Location;
+import com.sg.superherosightings.Entity.JPAEntities.Organisation;
 import com.sg.superherosightings.TestApplicationConfiguration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestApplicationConfiguration.class)
@@ -15,8 +20,42 @@ public class HeroRepoTest {
     @Autowired
     HeroRepository heroRepo;
 
+    @Autowired
+    SuperPowerRepository spRepo;
+
+    @Autowired
+    OrganisationRepository orgRepo;
+
+    @Autowired
+    SightingRepository sightRepo;
+
+    @Autowired
+    LocationRepository locationRepository;
+
+//    @BeforeEach
+//    public void clearDB(){
+//        heroRepo.deleteAll();
+//        spRepo.deleteAll();
+//    }
+
     @Test
-    public void contextLoads() {
-        System.out.println(Arrays.toString(heroRepo.findAll().toArray()));
+    public void createAndSaveLocationAndOrganisation() {
+//        Location location = new Location();
+//        location.setAddress("address");
+//        location.setDescription("desc");
+//        location.setName("some place");
+//        location.setLatitude(0.02);
+//        location.setLongitude(0.021);
+//        Location newLoc = locationRepository.save(location);
+        Organisation org = new Organisation();
+//        org.setName("Extra Definitely Evil Inc");
+//        org.setDescription("an org");
+        org.setLocation(null);
+//        org.setTelephone("023094544532");
+        org.setMembers(new HashSet<>());
+        org.setId(1);
+//        Organisation newOrg = orgRepo.save(org);
+//        System.out.println(newOrg.toString());
+        System.out.println(Arrays.toString(heroRepo.findByOrganisation(org).toArray()));
     }
 }

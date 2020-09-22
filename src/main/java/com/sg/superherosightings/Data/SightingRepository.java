2e77d94d1f11665837418ca5e9931f14a099011b
Dani <dani@danipc.com>
2020-09-22 23:43:06 +0100

    0022: fix sql

--- src/main/java/com/sg/superherosightings/Data/SightingRepository.java
+++ src/main/java/com/sg/superherosightings/Data/SightingRepository.java
@@ -17,7 +17,7 @@ public interface SightingRepository extends JpaRepository<Sighting, Integer> {
     @Query(value = "SELECT h.* from hero h JOIN sighting ON sighting.hero_id = h.id WHERE sighting.location_id = :#{#location.id}", nativeQuery = true)
     List<Hero> findByLocation(@Param("location") Location location);
 
-    @Query(value = "SELECT * FROM location JOIN sighting ON sighting.location_id = location.id WHERE sighting.hero_id = :#{#hero.id}", nativeQuery = true)
+    @Query(value = "SELECT l.* FROM location l JOIN sighting ON sighting.location_id = l.id WHERE sighting.hero_id = :#{#hero.id}", nativeQuery = true)
     List<Location> findByHero(@Param("hero") Hero hero);
 
     @Query(value = "SELECT * FROM sighting WHERE sighting.date_of_sighting = :#{#date}", nativeQuery = true)
--- src/test/java/HeroRepoTest.java
+++ src/test/java/HeroRepoTest.java
@@ -2,8 +2,10 @@ import com.sg.superherosightings.Data.*;
 import com.sg.superherosightings.Entity.JPAEntities.Hero;
 import com.sg.superherosightings.Entity.JPAEntities.Location;
 import com.sg.superherosightings.Entity.JPAEntities.Organisation;
+import com.sg.superherosightings.Entity.JPAEntities.Sighting;
+import com.sg.superherosightings.Entity.JPAEntities.SuperPower;
 import com.sg.superherosightings.TestApplicationConfiguration;
-import org.junit.jupiter.api.BeforeEach;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import org.junit.jupiter.api.BeforeEach;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestApplicationConfiguration.class)
@@ -31,11 +34,12 @@ public class HeroRepoTest {
 
     @Autowired
     LocationRepository locationRepository;
-
+//
 //    @BeforeEach
 //    public void clearDB(){
 //        heroRepo.deleteAll();
 //        spRepo.deleteAll();
+//
 //    }
 
     @Test
@@ -61,14 +65,48 @@ public class HeroRepoTest {
 
     @Test
     public void findAllHeroesByLocationTest(){
+//        LocalDate newDate = LocalDate.of(2020, 1, 21);
+//        Hero h = new Hero();
+//        SuperPower superpower = new SuperPower();
+//        superpower.setDescription("power description");
+//        superpower.setName("power name");
+//        spRepo.save(superpower);
+//        h.setName("hero name");
+//        h.setDescription("hero description");
+//        h.setVillain(true);
+//        h.setSuperpower(superpower);
+//        heroRepo.save(h);
         Location loc = new Location();
         loc.setAddress("some address");
         loc.setDescription("some loc description");
         loc.setLatitude(12.0);
         loc.setLongitude(45.0);
         loc.setName("location name");
-
+        locationRepository.save(loc);
+//        Sighting sight1 = new Sighting();
+//        sight1.setLocation(loc);
+//        sight1.setHero(h);
+//        sight1.setDate(newDate);
+//        sightRepo.save(sight1);
         System.out.println(Arrays.toString(sightRepo.findByLocation(loc).toArray()));
+    }
+    
+    @Test
+    public void findAllLocationsByHero(){
+        Hero h = new Hero();
+        SuperPower superpower = new SuperPower();
+        h.setName("hero name");
+        h.setDescription("hero description");
+        h.setVillain(true);
+        h.setSuperpower(superpower);
+        System.out.println(Arrays.toString(sightRepo.findByHero(h).toArray()));
+    }
+    
+    @Test
+    public void findSightingsByDate(){
+        LocalDate newDate = LocalDate.of(2020, 1, 21);
+
+        System.out.println(Arrays.toString(sightRepo.findByDate(newDate).toArray()));
     }
 
 

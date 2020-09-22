d21f1852c9b2f2755ce3cb657ea32f8d2b6c9099
Dani <dani@danipc.com>
2020-09-22 16:11:48 +0100

    0017: added custom queries

--- src/main/java/com/sg/superherosightings/Data/OrganisationRepository.java
+++ src/main/java/com/sg/superherosightings/Data/OrganisationRepository.java
@@ -3,11 +3,17 @@ package com.sg.superherosightings.Data;
 import com.sg.superherosightings.Entity.JPAEntities.Hero;
 import com.sg.superherosightings.Entity.JPAEntities.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrganisationRepository extends JpaRepository<Organisation, Integer> {
-//    List<Organisation> findByHero(Hero hero);
+    @Query(value="SELECT * FROM organisation JOIN hero_in_organisation ON hero_in_organisation.organisation_id = organisation.organisation_id WHERE hero_in_organisation.hero_id = :#{#hero.id}", nativeQuery = true)
+    List<Organisation> findByHero(@Param("hero") Hero hero);
+
+    @Query(value = "SELECT * FROM hero_in_organisation JOIN hero ON hero.id = hero_in_organisation.hero_id WHERE hero_in_organisation.organisation_id = :#{#organisation.id}", nativeQuery = true)
+    List<Hero> findByOrganisation(@Param("organisation") Organisation organisation);
 }
--- src/main/java/com/sg/superherosightings/Data/SightingRepository.java
+++ src/main/java/com/sg/superherosightings/Data/SightingRepository.java
@@ -5,6 +5,7 @@ import com.sg.superherosightings.Entity.JPAEntities.Location;
 import com.sg.superherosightings.Entity.JPAEntities.Sighting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,10 +14,15 @@ import java.util.List;
 @Repository
 public interface SightingRepository extends JpaRepository<Sighting, Integer> {
 
-    List<Hero> findByLocation(Location location);
-    List<Location> findByHero(Hero hero);
-    List<Sighting> findByDate(LocalDate date);
+    @Query(value = "SELECT * from hero JOIN sighting ON sighting.hero_id = hero.id WHERE sighting.location_id = :#{#location.id}")
+    List<Hero> findByLocation(@Param("location") Location location);
 
-//    @Query (value = "SELECT * FROM Sighting ORDER BY date DESC LIMIT 10", nativeQuery = true)
-//    List<Sighting> findTenRecentSightings();
+    @Query(value = "SELECT * FROM location JOIN sighting ON sighting.location_id = location.id WHERE sighting.hero_id = :#{#hero.id}")
+    List<Location> findByHero(@Param("hero") Hero hero);
+
+    @Query(value = "SELECT * FROM sighting WHERE sighting.date_of_sighting = :#{#sighting.date_of_sighting}")
+    List<Sighting> findByDate(@Param("date") LocalDate date);
+
+    @Query(value = "SELECT * FROM sighting ORDER BY date DESC LIMIT 10", nativeQuery = true)
+    List<Sighting> findTenRecentSightings();
 }

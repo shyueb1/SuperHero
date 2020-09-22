cb420fa23e18764b019b24937ed1ffd0290c3bdd
Dani <dani@danipc.com>
2020-09-22 11:16:24 +0100

    0006: Added Superpower

--- src/main/java/com/sg/superherosightings/Controller/MainController.java
+++ src/main/java/com/sg/superherosightings/Controller/MainController.java
@@ -1,7 +1,9 @@
 package com.sg.superherosightings.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class MainController {
 

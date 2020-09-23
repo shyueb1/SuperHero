161e4755a54af276486c4a7b6ad80da6eef7374c
meganhalp <meganhalp@gmail.com>
2020-09-23 09:09:00 +0100

    0024: update card with space between card and edge of browser

--- src/React/src/components/NewsFeed.js
+++ src/React/src/components/NewsFeed.js
@@ -20,7 +20,7 @@ const ItemCard = ({ sighting }) => {
   return (
     <Col sm={6}>
       <Row>
-        <Card className="text-center">
+        <Card className="text-center mr-4 mt-4 ml-4 mb-4">
           <Card.Title>{sighting.hero}</Card.Title>
           <Card.Text>{sighting.location}</Card.Text>
           <Card.Text>{sighting.date}</Card.Text>

8e58648c8449b198689f567c28dee93def2b3b5a
meganhalp <meganhalp@gmail.com>
2020-09-24 22:06:23 +0100

    0038: add organisation form completed

--- src/React/src/pages/Organisation.js
+++ src/React/src/pages/Organisation.js
@@ -32,22 +32,74 @@ class Organisation extends React.Component {
                 },
                 "telephone": ""
             }
-        ]
+        ],
+        locations: [
+            {
+                id: "",
+                name: "",
+                description: "",
+                address: "",
+                latitude: "",
+                longitude: "",
+            },
+        ],
+        allLocations: []
     };
+    handleSubmitForm = (values) => {
+        let subName = values.name;
+        let subDescrip = values.description;
+        let subLocation = values.location;
+        let locationId;
+        this.state.locations.forEach(l => {
+            if (l.name === subLocation) {
+                locationId = l.id;
+            }
+        });
+        let subTelephone = values.telephone;
+        let subMission = {
+            "name": subName,
+            "description": subDescrip,
+            "location": {
+                "id": locationId
+            },
+            "telephone": subTelephone
+        };
+        fetch(SERVICE_URL + "/organisation/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(subMission),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Success:", data);
+                this.loadLocationsAndOrganisations();
+            })
+            .catch((error) => {
+                console.log("Error:", error);
+            });
+
+    }
 
     componentDidMount() {
         console.log("App is now mounted");
-        this.loadOrganisations();
+        this.loadLocationsAndOrganisations();
     }
 
-    loadOrganisations() {
+    loadLocationsAndOrganisations() {
         this.setState({ loading: true });
-        console.log("Loading organisations");
-        fetch(SERVICE_URL + "organisation")
-            .then(data => data.json())
-            .then(data => this.setState(
-                { organisations: data, loading: false }
-            ));
+        console.log("Loading locations and organisations");
+        Promise.all([fetch(SERVICE_URL + "location"), fetch(SERVICE_URL + "organisation")])
+            .then(([response1, response2]) => {
+                return Promise.all([response1.json(), response2.json()])
+            })
+            .then(([data1, data2]) => {
+                this.setState({
+                    locations: data1, loading: false,
+                    organisations: data2, allLocations: data1.map(d => d.name)
+                })
+            });
     }
 
     handleDeleteOrganisation = (event) => {
@@ -63,7 +115,7 @@ class Organisation extends React.Component {
             },
             body: JSON.stringify({ "id": organisationId })
         })
-            .then(data => { this.loadOrganisations(); })
+            .then(data => { this.loadLocationsAndOrganisations(); })
             .catch(error => { console.error('Error: ', error); });
     }
 
@@ -72,7 +124,6 @@ class Organisation extends React.Component {
             <Container fluid style={{ padding: 0 }}>
                 {/* <Container fluid> */}
                 <Header />
-
                 <Row>
                     <Col sm={2}>
                         <Sidebar />
@@ -80,7 +131,11 @@ class Organisation extends React.Component {
 
                     <Col sm={9}>
                         <Row>
-                            <AddOrganisationForm />
+                            <AddOrganisationForm
+                                handleSubmitForm={this.handleSubmitForm}
+                                allLocations={(this.state.allLocations)}
+                                loadAll={this.loadLocationsAndOrganisations}
+                                locations={this.state.locations} />
                         </Row>
                         <Row>
                             <TableOrganisation organisations={this.state.organisations}
